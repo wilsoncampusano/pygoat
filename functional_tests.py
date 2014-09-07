024123244a4b68b073d00609714a218131c1f584
wilson campusano <wilsoncampusanojorge@gmail.com>
2014-09-07 11:53:03 -0400

    modified view to merge variable into the template, more clear functional test

--- functional_tests.py
+++ functional_tests.py
@@ -34,13 +34,17 @@ class NewVisitor(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 #there is still a textbox iviting her to add another item. she
 #enter "use peacock feathers to make fly"
 #the page updates again, and now shos both item on her list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('2: Use peacock feathers to make fly',[row.text for row in rows])
 		self.fail('Finish the test!!')
 #edit wonder where the site will remember her list. then she sees that the site has generated a unique url
 #for her --  the is son explanatory text to that reflect
