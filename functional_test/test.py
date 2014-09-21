afee101bbc2468781d422ea1681695547aab9c79
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-09-21 18:11:52 -0400

    hack funtional test to be able to test staging

--- functional_test/test.py
+++ functional_test/test.py
@@ -3,8 +3,23 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitor(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -23,7 +38,7 @@ class NewVisitor(StaticLiveServerTestCase):
 #edit has hear about a cool new online to-do app. she goes
 #to check out its homepage
 		
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 #she notice the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
 		#header_text = self.browser.find_element_by_tag_name('h1').text
@@ -64,7 +79,7 @@ class NewVisitor(StaticLiveServerTestCase):
 
 #Francis visits the home page. There is no sign of Edith's list
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy a peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -85,7 +100,7 @@ class NewVisitor(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#she noticies the input box is necely centered
