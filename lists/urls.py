a4786d493a52e05680ddaf18fac3ecd27ba14043
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-09-20 20:45:07 -0400

    new file:   lists/urls.py app specific urls 	modified:   superlists/urls.py

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='new_list')
+)
