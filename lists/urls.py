75836fee851685e37ba8757499a722b22923c5a8
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-01 09:13:24 -0400

    pep8

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
 )
