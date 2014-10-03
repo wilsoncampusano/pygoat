a59a08ff6c835f51baa0d0bd99b990c122225d3b
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-02 21:04:05 -0400

    ft moved into their own file. now it can be independently tested

---
+++ functional_test/test_list_item_validation.py
@@ -0,0 +1,8 @@
+from unittest import skip
+from .base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        pass
--- functional_test/test_simple_list_creation.py
+++ functional_test/test_simple_list_creation.py
@@ -1,38 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import sys
-
-
-class FunctionalTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
+from .base import FunctionalTest
 
 
 class NewVisitor(FunctionalTest):
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrive_it_latter(self):
         self.browser.get(self.server_url)
@@ -72,19 +43,3 @@ class NewVisitor(FunctionalTest):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-
-class StylesFunctionalTest(FunctionalTest):
-    def test_layout_and_styling(self):
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
-
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
