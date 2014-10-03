88c392146ac75ab9a85a2fdf82b3255edf1f3ee7
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-02 21:19:14 -0400

    unit test moved into their own directory

--- functional_test/test_list_item_validation.py
+++ functional_test/test_list_item_validation.py
@@ -1,8 +1,29 @@
-from unittest import skip
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    @skip
+
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        error = self.browser.find_element_by_id('.has-error')
+
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk \n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        error = self.browser.find_element_by_id('.has-error')
+
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea \n')
+
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
