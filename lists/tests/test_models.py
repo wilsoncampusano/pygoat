1cab6cd6f620a8e6bb209a656323d121c4b9391b
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-02 21:24:35 -0400

    split out unit test into two files

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,50 +1,5 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from lists.models import Item, List
-from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
-
-
-class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        list_ = List.objects.create()
-        response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_only_items_for_that_list(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
-
-        other_list_ = List.objects.create()
-        Item.objects.create(text='other list item 1', list=other_list_)
-        Item.objects.create(text='other list item 2', list=other_list_)
-
-        response = self.client.get('/lists/%d/' % (list_.id,))
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
-
-        self.assertNotContains(response, 'other list item 1')
-        self.assertNotContains(response, 'other list item 2')
-
-    def test_passes_correct_list_to_template(self):
-        correct_list = List.objects.create()
-        response = self.client.get('/lists/%d/' % (correct_list.id,))
-        self.assertEqual(response.context['list'], correct_list)
 
 
 class ListAndItemModelTest(TestCase):
