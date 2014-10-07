679d663d0ce13157901c539134d1e7b363c44894
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-06 20:49:17 -0400

    test refactor

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -84,4 +84,4 @@ class NewListTest(TestCase):
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={"item_text": ''})
         self.assertEqual(List.objects.count(), 0)
-        self.assertEqual(Item.objects.count(), 0) 
+        self.assertEqual(Item.objects.count(), 0)
